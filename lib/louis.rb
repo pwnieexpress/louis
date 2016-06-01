ddcb477454a2ec26d41f199dc764b03a8ea95825
Joshua Stone <joshua.gage.stone@gmail.com>
2016-06-01 19:11:49 -0400

    Make Louis.lookup accept only strings as valid input param to enforce spec

--- lib/louis.rb
+++ lib/louis.rb
@@ -32,16 +32,18 @@ module Louis
   # @param [String] mac
   # @return [String]
   def self.lookup(mac)
-    encoded_mac = mac_to_num(mac) & IGNORED_BITS_MASK
+    if mac.is_a? String
+      encoded_mac = mac_to_num(mac) & IGNORED_BITS_MASK
 
-    lookup_table.each do |mask, table|
-      prefix = (encoded_mac & calculate_mask(nil, mask)).to_s
-      if table.include?(prefix)
-        vendor = table[prefix]
-        return {'long_vendor' => vendor['l'], 'short_vendor' => vendor['s']}
+      lookup_table.each do |mask, table|
+        prefix = (encoded_mac & calculate_mask(nil, mask)).to_s
+        if table.include?(prefix)
+          vendor = table[prefix]
+          return {'long_vendor' => vendor['l'], 'short_vendor' => vendor['s']}
+        end
       end
     end
-    
+
     {'long_vendor' => 'Unknown', 'short_vendor' => 'Unknown'}
   end
 end
